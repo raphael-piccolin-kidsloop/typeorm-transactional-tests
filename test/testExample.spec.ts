86f02e9f6674e1e8fbe33c8931cbc36a77bd82dc
Raphael Piccolin <raphael.piccolin@kidsloop.live>
2022-02-15 12:16:31 +0000

    test: added tests for transaction() monkey patch

--- test/testExample.spec.ts
+++ test/testExample.spec.ts
@@ -1,4 +1,12 @@
-import { Connection, createConnection, getConnection, Repository } from 'typeorm';
+import {
+  Connection,
+  createConnection,
+  Entity,
+  EntityManager,
+  getConnection,
+  getManager,
+  Repository,
+} from 'typeorm';
 import TransactionalTestContext from '../src/transactionalTestContext';
 import Person from './entities/person.entity';
 
@@ -7,8 +15,8 @@ describe('transactional test example - SQL lite', () => {
   let repository: Repository<Person>;
   let transactionalContext: TransactionalTestContext;
 
-  beforeEach(async () => {
-    await createConnection({
+  beforeAll(async () => {
+    connection = await createConnection({
       type: 'sqlite',
       name: 'default',
       synchronize: true,
@@ -19,20 +27,54 @@ describe('transactional test example - SQL lite', () => {
   });
 
   beforeEach(async () => {
-    connection = getConnection();
     repository = connection.getRepository(Person);
     transactionalContext = new TransactionalTestContext(connection);
     await transactionalContext.start();
-    await Promise.all([
-      repository.save(new Person({ name: 'Aragorn' })),
-      repository.save(new Person({ name: 'Legolas' })),
-    ]);
   });
 
   describe('rollback transaction', () => {
+    beforeEach(async () => {
+      await Promise.all([
+        repository.save(new Person({ name: 'Aragorn' })),
+        repository.save(new Person({ name: 'Legolas' })),
+      ]);
+    });
+
     it('the database should be empty', async () => {
       expect(await repository.count()).toEqual(2);
       await transactionalContext.finish();
+      expect(await repository.count()).toEqual(0);
+    });
+  });
+
+  describe('nested transaction', () => {
+    it('does not error when it encounters a nested transaction', async () => {
+      await getManager().transaction(async manager => {
+        await manager.save(new Person({ name: 'Aragorn' }));
+        await manager.save(new Person({ name: 'Legolas' }));
+      });
+      await transactionalContext.finish();
+    });
+
+    it('treats nested transaction as part of the test transaction', async () => {
+      await expect(
+        getManager().transaction(async manager => {
+          await manager.save(new Person({ name: 'Gimli' }));
+          throw new Error('this error triggers a rollback');
+        }),
+      ).rejects.toThrow('this error triggers a rollback');
+      expect(await repository.count()).toEqual(1);
+      await transactionalContext.finish();
+    });
+
+    it('resets EntityManager.prototype.transaction() after context ends', async () => {
+      await transactionalContext.finish();
+      await expect(
+        getManager().transaction(async manager => {
+          await manager.save(new Person({ name: 'Pippin' }));
+          throw new Error('this error triggers a rollback');
+        }),
+      ).rejects.toThrow('this error triggers a rollback');
       expect(await repository.count()).toEqual(0);
     });
   });
